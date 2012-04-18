29a1f129c03e894b3fd622fde7f54011f1a72f70
Matt Fordham <matt@revolvercreative.com>
2012-04-18 15:02:54 -0700

    Updated README

--- export_to_cloud.gemspec
+++ export_to_cloud.gemspec
@@ -10,17 +10,17 @@ Gem::Specification.new do |s|
   s.authors     = ["Matt Fordham"]
   s.email       = ["matt@revolvercreative.com"]
   s.homepage    = "https://github.com/mattfordham/export_to_cloud"
-  s.summary     = "TODO: Summary of ExportToCloud."
-  s.description = "TODO: Description of ExportToCloud."
+  s.summary     = "Exports all rows of a Rails model as a CSV file to Amazon S3"
+  s.description = "Adds a class method to all ActiveRecord models in Rails that exports all rows in a single CSV file to an Amazon S3 bucket"
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
 
-  s.add_dependency "rails", "~> 3.2.3"
-
+  s.add_runtime_dependency "fog"
+  
+  s.add_development_dependency "rails", "~> 3.2.3"
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails"
   s.add_development_dependency "guard"
   s.add_development_dependency "guard-rspec"
-  s.add_development_dependency "fog"
   s.add_development_dependency "ruby-debug19"
 end
--- lib/export_to_cloud.rb
+++ lib/export_to_cloud.rb
@@ -2,51 +2,60 @@ require 'csv'
 require 'fog'
 
 module ExportToCloud
-  
+
   mattr_accessor :aws_access_key_id, :aws_secret_access_key, :s3_bucket, :path
-  
+
   def self.included(base)
     base.extend(ClassMethods)
   end
-  
+
   module ClassMethods
 
     def export_to_cloud
-      columns = Person.column_names.collect {|x| x.titleize}
-      
-      file = File.open("#{Rails.root}/tmp/#{self.to_s}.csv", 'w') do |f|
-        f.puts columns.to_csv
-        self.find_each do |record|
-          values = []
-          record.attribute_names.each do |attr_name|
-            value = record[attr_name]
-            value = value.strftime('%Y-%m-%d at %I:%M%p') if value.respond_to?(:strftime)
-            values.push value
-          end
-          f.puts values.to_csv      
-        end
-      end
-
-      connection = Fog::Storage.new({
-        :provider                 => 'AWS',
-        :aws_access_key_id        => ExportToCloud.aws_access_key_id,
-        :aws_secret_access_key    => ExportToCloud.aws_secret_access_key
-      })
-      
-      directory = connection.directories.get(ExportToCloud.s3_bucket)
-      path = "#{ExportToCloud.path}" if ExportToCloud.path.present?
-      filename = "#{self.to_s.pluralize.downcase}-#{Time.now.to_formatted_s(:number)}.csv"
-      
-      file = directory.files.create(
-        :key    => "#{path}#{filename}",
-        :body   => File.open("#{Rails.root}/tmp/#{self.to_s}.csv"),
-        :public => true
-      )
-      
-      file.save
-      
+      ExportToCloud::create_local_csv_file(self)
+      ExportToCloud::send_to_s3(self)
       File.delete("#{Rails.root}/tmp/#{self.to_s}.csv")
     end
+
   end
+
+  def self.create_local_csv_file(model_to_export)
+    columns = model_to_export.column_names.collect {|x| x.titleize}
+
+    file = File.open("#{Rails.root}/tmp/#{model_to_export.to_s}.csv", 'w') do |f|
+      f.puts columns.to_csv
+      model_to_export.find_each do |record|
+        values = []
+        record.attribute_names.each do |attr_name|
+          value = record[attr_name]
+          value = value.strftime('%Y-%m-%d at %I:%M%p') if value.respond_to?(:strftime)
+          values.push value
+        end
+        f.puts values.to_csv      
+      end
+    end
+  end
+  
+  def self.send_to_s3(model_to_export)
+    connection = Fog::Storage.new({
+      :provider                 => 'AWS',
+      :aws_access_key_id        => ExportToCloud.aws_access_key_id,
+      :aws_secret_access_key    => ExportToCloud.aws_secret_access_key
+    })
+
+    directory = connection.directories.get(ExportToCloud.s3_bucket)
+    path = "#{ExportToCloud.path}" if ExportToCloud.path.present?
+    filename = "#{model_to_export.to_s.pluralize.downcase}-#{Time.now.to_formatted_s(:number)}.csv"
+
+    file = directory.files.create(
+      :key    => "#{path}#{filename}",
+      :body   => File.open("#{Rails.root}/tmp/#{model_to_export.to_s}.csv"),
+      :public => true
+    )
+
+    file.save
+  end
+  
+end
 
 ActiveRecord::Base.send(:include, ExportToCloud)
